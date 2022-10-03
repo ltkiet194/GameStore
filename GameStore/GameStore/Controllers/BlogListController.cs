18606c10bda8129f71d5fe43dc9b10490bfec1ad
Kiet Luong <ltkiet194@gmail.com>
2022-10-03 22:19:59 +0700

    New Blog List

---
+++ GameStore/GameStore/Controllers/BlogListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class BlogListController : Controller
+    {
+        // GET: BlogArticle
+        public ActionResult BlogList()
+        {
+            return View();
+        }
+    }
+}
